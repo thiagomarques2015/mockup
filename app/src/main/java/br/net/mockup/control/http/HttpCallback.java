b66f9b6a88f846b379c5bac49b85526e1ecf9946
Thiago Marques <thiago@operahouse.com.br>
2016-05-30 17:18:03 -0400

    Atualizado a interface de callback da classe de conexão do tipo JSON

--- app/src/main/java/br/net/mockup/control/http/HttpCallback.java
+++ app/src/main/java/br/net/mockup/control/http/HttpCallback.java
@@ -3,7 +3,7 @@ package br.net.mockup.control.http;
 /**
  * Created by Thiago on 18/11/2015.
  */
-class HttpCallback {
+public class HttpCallback {
     public interface PostJSONListener{
         void onSuccess(int statusCode, String response, byte[] responseBody);
         void onFailure(int statusCode,  String response, byte[] responseBody, Throwable error);
